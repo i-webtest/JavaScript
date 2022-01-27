062a8622c5ab6fb289dc9ae833b10f8bae260a42
i-webtest <stasbass80@gmail.com>
2022-01-27 22:06:20 +0300

    Выполнен урок 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log();
+var title;
+var screens;
+var screenPrice;
+var rollback;
+var fullPrice;
+var adaptive;
+
+alert("GIT - это что-то мега сложное и непонятное!!!");
+
+console.log("JavaScript");
