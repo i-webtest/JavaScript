e802c26c280d2f103fb0ab994e23667b3c2ea32e
i-webtest <stasbass80@gmail.com>
2022-01-28 23:19:07 +0300

    Урок №2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-let title, screens, screenPrice, rollback, fullPrice, adaptive;
+let title = "Javascript";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 450;
+let rollback = 10;
+let fullPrice = 30000;
+let adaptive = true;
 
-alert("GIT - это что-то мега сложное и непонятное!!!");
-
-console.log("JavaScript");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log(
+  "Стоимость верстки экранов " + screenPrice + " рублей/долларов/гривен/юани"
+);
+console.log(
+  "Стоимость разработки сайта " + fullPrice + " рублей/долларов/гривен/юани"
+);
+console.log(screens.toLowerCase());
+console.log(screens.split(", "));
+console.log(fullPrice * (rollback / 100));
