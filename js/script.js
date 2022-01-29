5ff7d7be8db80aff11c65936e377338180dc6623
i-webtest <stasbass80@gmail.com>
2022-01-29 12:35:10 +0300

    исправил мелкие недочёты

--- js/script.js
+++ js/script.js
@@ -5,16 +5,13 @@ let rollback = 10;
 let fullPrice = 30000;
 let adaptive = true;
 
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-console.log(screens.length);
+console.log("Тип данных значения переменной title: " + typeof title);
+console.log("Тип данных значения переменной fullPrice: " + typeof fullPrice);
+console.log("Тип данных значения переменной adaptive: " + typeof adaptive);
+console.log("Длинна строки переменной screens: " + screens.length);
+console.log("Стоимость верстки экранов " + screenPrice + " рублей");
+console.log("Стоимость разработки сайта " + fullPrice + " рублей");
+console.log(screens.toLowerCase().split(", "));
 console.log(
-  "Стоимость верстки экранов " + screenPrice + " рублей/долларов/гривен/юани"
+  "Процент отката посреднику за работу: " + fullPrice * (rollback / 100)
 );
-console.log(
-  "Стоимость разработки сайта " + fullPrice + " рублей/долларов/гривен/юани"
-);
-console.log(screens.toLowerCase());
-console.log(screens.split(", "));
-console.log(fullPrice * (rollback / 100));
