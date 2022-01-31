c0e7b27c009f7d1f7df8d1663b6949e5bd2ea3f3
i-webtest <stasbass80@gmail.com>
2022-01-31 20:08:22 +0300

    выполнил урок №3

--- js/script.js
+++ js/script.js
@@ -1,17 +1,62 @@
-let title = "Javascript";
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 450;
-let rollback = 10;
-let fullPrice = 30000;
-let adaptive = true;
+"use strict";
+// Спрашиваем у пользователя “Как называется ваш проект?” и результат сохраняем в переменную title
+let title = prompt("Как называется ваш проект?", "");
+console.log("Ваш проект называется: " + title);
 
-console.log("Тип данных значения переменной title: " + typeof title);
-console.log("Тип данных значения переменной fullPrice: " + typeof fullPrice);
-console.log("Тип данных значения переменной adaptive: " + typeof adaptive);
-console.log("Длинна строки переменной screens: " + screens.length);
-console.log("Стоимость верстки экранов " + screenPrice + " рублей");
-console.log("Стоимость разработки сайта " + fullPrice + " рублей");
-console.log(screens.toLowerCase().split(", "));
-console.log(
-  "Процент отката посреднику за работу: " + fullPrice * (rollback / 100)
+// Спросить у пользователя “Какие типы экранов нужно разработать?” сохранить в переменную screens
+let screens = prompt(
+  "Какие типы экранов нужно разработать?",
+  "Простые, Сложные, Интерактивные"
 );
+console.log("Нужно разработать " + screens + " типы экранов");
+
+// Спросить у пользователя “Сколько будет стоить данная работа?” и сохранить в переменную screenPrice
+let screenPrice = prompt("Сколько будет стоить данная работа?", 12000);
+console.log("Данная работа будет стоить: " + screenPrice + " рублей");
+
+// Спросить у пользователя “Нужен ли адаптив на сайте?” и сохранить данные в переменной adaptive
+let adaptive = prompt("Нужен ли адаптив на сайте?");
+if (adaptive == "да") {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+// Спросить у пользователя по 2 раза каждый вопрос и записать ответы в разные переменные
+// 1. “Какой дополнительный тип услуги нужен?”
+// 2. “Сколько это будет стоить?”
+let service1 = prompt("Какой дополнительный тип услуги нужен?");
+console.log("Дополнительная услуга: " + service1);
+
+let servicePrice1 = prompt("Сколько это будет стоить?");
+console.log("Стоимость: " + servicePrice1);
+
+let service2 = prompt("Какой дополнительный тип услуги нужен?");
+console.log("Дополнительная услуга: " + service2);
+
+let servicePrice2 = prompt("Сколько это будет стоить?");
+console.log("Стоимость: " + servicePrice2);
+
+// Вычислить итоговую стоимость работы учитывая стоимость верстки экранов и дополнительных услуг и результат занести в
+//переменную fullPrice
+let fullPrice =
+  Number(screenPrice) + Number(servicePrice1) + Number(servicePrice2);
+alert("Итоговая стоимость: " + fullPrice);
+console.log("Итоговая стоимость: " + fullPrice);
+
+// Объявить переменную servicePercentPrice и занести в нее итоговую стоимость за вычетом отката посреднику, округлив
+//результат в большую сторону. Вывести servicePercentPrice в консоль.
+let rollback = 10;
+let servicePercentPrice = fullPrice * (rollback / 100);
+console.log("Откат посреднику: " + Math.ceil(servicePercentPrice));
+
+// Написать конструкцию условий
+if (fullPrice > 30000) {
+  console.log("Даем скидку в 10%");
+} else if (15000 < fullPrice && fullPrice < 30000) {
+  console.log("Даем скидку в 5%");
+} else if (15000 > fullPrice && fullPrice > 0) {
+  console.log("Скидка не предусмотрена");
+} else {
+  console.log("Что-то пошло не так");
+}
